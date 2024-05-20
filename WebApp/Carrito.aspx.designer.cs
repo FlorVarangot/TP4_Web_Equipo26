d0ded386635bae1dd779241b5644923b4b94a17f
Damian Di Giovanni <demian-dg@hotmail.com>
2024-05-19 23:41:04 -0300

    Se agrega total, corregir etiqueta

--- WebApp/Carrito.aspx.designer.cs
+++ WebApp/Carrito.aspx.designer.cs
@@ -22,5 +22,14 @@ namespace WebApp
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView dgvArticulos;
+
+        /// <summary>
+        /// Control lblSubtotal.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblSubtotal;
     }
 }
